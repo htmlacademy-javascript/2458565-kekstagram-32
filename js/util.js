c87734f065f8c43173873cd1210c5c97f721e518
ANNAstasiyaBelskaya <belskayanastasiya@yandex.ru>
2024-07-16 21:38:32 +0400

    module5-task1

---
+++ js/util.js
@@ -0,0 +1,10 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a,b));
+  const upper = Math.floor(Math.max(a,b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+export {getRandomPositiveInteger, getRandomArrayElement};
