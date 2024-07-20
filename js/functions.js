cf700d52ed8f69b31f5960e7af80c2de9869b304
ANNAstasiyaBelskaya <belskayanastasiya@yandex.ru>
2024-07-20 21:05:21 +0400

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -24,7 +24,7 @@ getPalindrome('ДоВоД');//true
 getPalindrome('топот'); //true
 getPalindrome('А роза упала на лапу Азора'); //true
 
-//3-1 (доп.)
+//3
 
 const getNumber = function(string) {
   string = String(string);
@@ -46,5 +46,29 @@ getNumber('а я томат'); // NaN
 getNumber(2023); // 2023
 getNumber(-1); // 1
 getNumber(1.5); // 15
+
+// 5.2
+function transformToMinutes (string) {
+  const splitedString = string.split(':');
+  const timeInMinutes = splitedString.reduce((hour, minutes) => (hour * 60) + +minutes, 0);
+  return timeInMinutes;
+}
+
+function getTime (workStart, workEnd, meetingStart, meetingLength) {
+  const workStartInMinutes = transformToMinutes(workStart);
+  const workEndInMinutes = transformToMinutes(workEnd);
+  const meetingStartInMinutes = transformToMinutes(meetingStart);
+
+  if (meetingStartInMinutes >= workStartInMinutes && (workEndInMinutes - meetingStartInMinutes >= meetingLength)) {
+    return true;
+  }
+  return false;
+}
+
+getTime('08:00', '17:30', '14:00', 90); // true
+getTime('8:0', '10:0', '8:0', 120);// true
+getTime('08:00', '14:30', '14:00', 90); // false
+getTime('14:00', '17:30', '08:0', 90);// false
+getTime('8:00', '17:30', '08:00', 900); // false
 
 
